d9c041910205969bc42448367adfb5e842ffc9eb
Anirban Banerjee <abanerjee763@outlook.com>
2024-01-21 16:03:33 +0530

    SOme changes. Closes #9

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className='fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20'>
             <div className='flex flex-row gap-3 items-center'>
                 <Image src="/horseLogo.jpg" alt='logo' width={40} height={40} className='w-full h-full object-contain rounded-full' />
-                <h1 className='text-white text-[25px] font-bold'>FINANCIA</h1>
+                <h1 className='text-white text-[25px] font-bold'>{process.env.API_NAME}</h1>
             </div>
             <div className='flex flex-row gap-5 mb-2'>
                 {Socials.map((social) => (
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+
+
+
 module.exports = {
-    cssModules: true,
-      cssLoaderOptions: {
-    importLoaders: 1,
-   localIdentName: "[]",
-  },
- };
+    env: {
+        DOMAIN : 'https://localhost:5006',
+        API_NAME : "FINANCIA",
+        DISQUS_SHORTNAME : 'seoblog-ucno9dvq83',
+        GOOGLE_CLIENT_ID : '76720414364-e8auk97b1h9q8d9o5ck58ohiug61ddj1.apps.googleusercontent.com'
+        
+    },
+}
